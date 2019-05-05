c618f80150f63a21fa0ce86db92aed68328964e7
Michaël Zasso <targos@protonmail.com>
2019-05-05 12:51:21 +0200

    update node zsh commands

--- zsh/node.sh
+++ zsh/node.sh
@@ -4,7 +4,7 @@
 alias ngt='tools/test.py -J `git show --name-only --pretty="" | grep 'test/'`'
 
 # compile and test
-alias mtv="make test -j8 V="
+alias mtv="make test -j12 V="
 
 alias ntw="NODE_OPTIONS='--trace-warnings'"
 
@@ -12,14 +12,14 @@ alias ntw="NODE_OPTIONS='--trace-warnings'"
 function nbd {
   BRANCH=$1
   echo "Running branch-diff for $BRANCH"
-  branch-diff ${BRANCH}-staging upstream/master --exclude-label=semver-major,dont-land-on-${BRANCH},backport-requested-${BRANCH},backported-to-${BRANCH} --filter-release --format=simple
+  branch-diff ${BRANCH}-staging upstream/master --exclude-label=semver-major,dont-land-on-${BRANCH},backport-requested-${BRANCH},backported-to-${BRANCH},backport-blocked-${BRANCH} --filter-release --format=simple
 }
 
 # branch-diff and apply
 function nbda {
   BRANCH=$1
   echo "Running branch-diff and cherry-picking for $BRANCH"
-  branch-diff ${BRANCH}-staging upstream/master --exclude-label=semver-major,dont-land-on-${BRANCH},backport-requested-${BRANCH},backported-to-${BRANCH} --filter-release --format=sha --reverse | xargs git cherry-pick
+  branch-diff ${BRANCH}-staging upstream/master --exclude-label=semver-major,dont-land-on-${BRANCH},backport-requested-${BRANCH},backported-to-${BRANCH},backport-blocked-${BRANCH} --filter-release --format=sha --reverse | xargs git cherry-pick
 }
 
 # update canary branch
